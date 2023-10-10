ca272edbba595ed62c105cc6c0faef8708cb94b1
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-10 11:50:52 -0600

    05

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,10 +1,11 @@
 import { countries } from "../../data/countries";
+import styles from './Form.module.css'
 
 export default function Form() {
   return (
-    <form>
+    <form className={styles.form}>
 
-        <div>
+        <div className={styles.field}>
             <label htmlFor="city">Ciudad:</label>
             <input
                 id="city"
@@ -14,7 +15,7 @@ export default function Form() {
             />
         </div>
 
-        <div>
+        <div className={styles.field}>
             <label htmlFor="city">País:</label>
             <select>
                 <option value="">-- Seleccione un País ---</option>
@@ -27,7 +28,7 @@ export default function Form() {
             </select>
         </div>
 
-        <input type="submit" value='Consultar Clima' />
+        <input className={styles.submit} type="submit" value='Consultar Clima' />
     </form>
   )
 }
