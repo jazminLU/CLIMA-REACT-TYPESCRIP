c50d6b79369793501ada1a00fed398243741408d
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-11 11:56:18 -0600

    09

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,8 +1,19 @@
+import axios from 'axios'
+import { SearchType } from '../types'
 
 export default function useWeather() {
 
-    const fetchWeather = () => {
-        console.log('Consultando...')
+    const fetchWeather = async (search: SearchType) => {
+
+        const appId = 'b45f484d35c875c7a973ea5bf8f7dc89'
+        try {
+            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+            const {data} = await axios(geoUrl)
+            console.log(data)
+
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return {
