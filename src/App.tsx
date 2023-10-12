a77fb31293b74e599072f689a66721e56b511ae2
Codigo Con Juan <blackmaxxgdl18@hotmail.com>
2023-10-11 19:18:47 -0600

    21

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styles from "./App.module.css"
+import Alert from "./components/Alert/Alert"
 import Form from "./components/Form/Form"
 import Spinner from "./components/Spinner/Spinner"
 import WeatherDetail from "./components/WeatherDetail/WeatherDetail"
@@ -6,19 +7,16 @@ import useWeather from "./hooks/useWeather"
 
 function App() {
 
-  const { weather, loading, fetchWeather, hasWeatherData } = useWeather()
+  const { weather, loading, notFound, fetchWeather, hasWeatherData } = useWeather()
 
   return (
     <>
         <h1 className={styles.title}>Buscador de Clima</h1>
-
         <div className={styles.container}>
-            <Form 
-              fetchWeather={fetchWeather}
-            />
+            <Form fetchWeather={fetchWeather} />
             {loading && <Spinner />}
             {hasWeatherData && <WeatherDetail weather={weather} />}
-       
+            {notFound && <Alert>Ciudad No Encontrada</Alert> }
         </div>
     </>
   )
--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -51,6 +51,7 @@ export default function useWeather() {
 
     const [weather, setWeather] = useState<Weather>(initialState)
     const [loading, setLoading] = useState(false)
+    const [notFound, setNotFound] = useState(false)
 
     const fetchWeather = async (search: SearchType) => {
         const appId = import.meta.env.VITE_API_KEY
@@ -60,6 +61,12 @@ export default function useWeather() {
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
             const {data} = await axios(geoUrl)
             
+            // Comprobar si existe
+            if(!data[0]) {
+                setNotFound(true)
+                return
+            }
+
             const lat = data[0].lat
             const lon = data[0].lon
 
@@ -104,7 +111,9 @@ export default function useWeather() {
     return {
         weather,
         loading,
+        notFound,
         fetchWeather,
-        hasWeatherData
+        hasWeatherData,
+
     }
 }
